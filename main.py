65c75247f63d5092e0c80e77ff9c2fb84a6deee1
Wamy <25761442+Wamy-Dev@users.noreply.github.com>
2025-05-05 17:05:27 -0500

    chore: better log formatting

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",
+    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
 )
 logging.getLogger("httpx").setLevel(logging.WARNING)
 
